282f8c84236d89a0526fee3bcc542e349d619bb7
faisel-burqawi <faisel.burqawi@gmail.com>
2018-06-25 21:25:17 -0400

    Added error handling

--- server.js
+++ server.js
@@ -35,6 +35,12 @@ app.get('/', (req,res) => {
   res.sendFile(__dirname + '/client/build/index.html')
 })
 
+app.use(function(req, res, next) {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
 app.use(function(err, req, res, next) {
   
   res.locals.message = err.message
