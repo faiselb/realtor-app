6f58f0ee0d58a2cf18e1c287421780d97c4537cd
faisel-burqawi <faisel.burqawi@gmail.com>
2018-06-25 21:40:48 -0400

    Added listing schema

--- db/schema.js
+++ db/schema.js
@@ -1,3 +1,21 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 mongoose.Promise = global.Promise
+
+const ListingSchema = new Schema(
+    {
+        listingAddress: String,
+        bedrooms: String,
+        bathrooms: String,
+        listingPrice: String,
+        yearbuilt:String,
+        listingdescription: String,
+    },
+    {
+        timestamps: {}
+    }
+)
+
+module.exports = {
+    ListingSchema
+}
