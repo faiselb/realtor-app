7ad95651dccc9015fbe938bb277ac327935def19
faisel-burqawi <faisel.burqawi@gmail.com>
2018-06-26 14:10:34 -0400

    added get route

--- controllers/realtorsController.js
+++ controllers/realtorsController.js
@@ -1,4 +1,18 @@
 const express = require('express')
 const Realtor = require('../db/models/Realtor')
 
+const router = express.Router()
+
+
+router.get('/', (request, response) => {
+    Realtor.find({})
+        .then((realtors) => {
+            response.json(realtors)
+        })
+        .catch((err) => console.log(err))
+})
+
+
+
+
 module.exports = router
