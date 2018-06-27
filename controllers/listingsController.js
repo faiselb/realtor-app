e9bfdf952c473f80119e1cf4a3c7225c49741242
faisel-burqawi <faisel.burqawi@gmail.com>
2018-06-27 12:21:43 -0400

    added get listings route

--- controllers/listingsController.js
+++ controllers/listingsController.js
@@ -5,4 +5,18 @@ const Listing = require('../db/models/Listing')
 const router = express.Router({ mergeParams: true })
 
 
+router.get('/', async (request, response) => {
+    try {
+        realtorId = request.params.realtorId
+        communityId = request.params.communityId
+        const realtor = await Realtor.findById(realtorId)
+        const community = await realtor.communities.id(communityId)
+        await response.json(community)
+    }
+    catch (err) {
+        console.log(err)
+    }
+})
+
+
 module.exports = router
