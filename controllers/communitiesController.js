a0527b3547f439690954372ea9e74545378f51c4
faisel-burqawi <faisel.burqawi@gmail.com>
2018-06-27 10:15:18 -0400

    create route to community

--- controllers/communitiesController.js
+++ controllers/communitiesController.js
@@ -15,6 +15,19 @@ router.get('/', (request, response) => {
         })
 })
 
+router.post('/', async (request, response) => {
+    try {
+        const realtor = await Realtor.findById(request.params.realtorId)
+        const newCommunity = await Community.create(request.body)
+        realtor.communities.push(newCommunity)
+        await realtor.save()
+        response.json(realtor)
+    }
+    catch (err) {
+        console.log(err)
+    }
+})
+
 
 
 module.exports = router
