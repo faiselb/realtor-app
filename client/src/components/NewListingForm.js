00ebe536d0281de4b15713f614f073f64818d469
faisel-burqawi <faisel.burqawi@gmail.com>
2018-06-29 14:09:17 -0400

    new listing form

---
+++ client/src/components/NewListingForm.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import SubmitButton from './SubmitButton'
+import styled from 'styled-components'
+
+function NewListingForm(props) {
+return (
+    <div>
+        <h6>Post Your New Listing</h6>
+        <div>
+            <form style={newFormStyle} onSubmit={props.createNewListing}>
+                <div>
+                    <label htmlFor="listingAddress">Address of Listing</label>
+                </div>
+                <div>
+                    <input onChange={props.handleListingChange} name="listingAddress" type="text" value={props.listing.listingAddress} />
+                </div>
+                <div>
+                    <label htmlFor="bedrooms">Number Of Bedrooms</label>
+                </div>
+                <div>
+                    <input onChange={props.handleListingChange} name="bedrooms" type="text" value={props.listing.bedrooms} />
+                </div>
+                <div>
+                    <label htmlFor="bathrooms">Number Of Bathrooms</label>
+                </div>
+                <div>
+                    <input onChange={props.handleListingChange} name="bathrooms" type="text" value={props.listing.bathrooms} />
+                </div>
+                <div>
+                    <label htmlFor="listingPrice">Listing Price</label>
+                </div>
+                <div>
+                    <input onChange={props.handleListingChange} name="listingPrice" type="text" value={props.listing.listingPrice} />
+                </div>
+                <div>
+                    <label htmlFor="yearbuilt">Year Built</label>
+                </div>
+                <div>
+                    <input onChange={props.handleListingChange} name="yearbuilt" type="text" value={props.listing.yearbuilt} />
+                </div>
+                
+                <div>
+                    <label htmlFor="listingdescription">Enter Listing Description</label>
+                </div>
+                <div>
+                    <input onChange={props.handleListingChange} name="listingdescription" type="text" value={props.listing.listingdescription} />
+                </div>
+
+                <div>
+                    <Button>Submit</Button>
+                </div>
+
+            </form>
+        </div>
+    </div>
+)
+}
+
+export default NewListingForm
+
+
+const newFormStyle = {
+
+width: 300,
+fontSize: 14,
+color: '#05B8CC',
+padding: '20px',
+
+}
+
+const Button = styled.button`
+border-radius: 5px;
+background-color: #05B8CC;
+color: #bfd964;
+border-radius: 5px;
+padding: 5px;
+margin: 8px;
+`
--- client/src/components/NewRealtor.js
+++ client/src/components/NewRealtor.js
@@ -3,43 +3,43 @@ import SubmitButton from './SubmitButton'
 import styled from 'styled-components'
 
 function NewRaeltor(props) {
-    return (
+return (
+    <div>
+
         <div>
+            <form style={newFormStyle} onSubmit={props.createRealtor}>
+                <div>
+                    <label htmlFor="name"> Realtor Name</label>
+                </div>
+                <div>
+                    <input className="form-input" onChange={props.handleChange} name="name" type="text" value={props.realtor.name} />
+                </div>
+                <div>
+                    <label htmlFor="address">Address</label>
+                </div>
+                <div>
+                    <input onChange={props.handleChange} name="address" type="text" value={props.realtor.address} />
+                </div>
+                <div>
+                    <label htmlFor="city">City</label>
+                </div>
+                <div>
+                    <input onChange={props.handleChange} name="city" type="text" value={props.realtor.city} />
+                </div>
+                <div>
+                    <label htmlFor="state">State</label>
+                </div>
+                <div>
+                    <input onChange={props.handleChange} name="state" type="text" value={props.realtor.state} />
+                </div>
+                <div>
+                    <SubmitButton />
+                </div>
 
-            <div>
-                <form style={newFormStyle} onSubmit={props.createRealtor}>
-                    <div>
-                        <label htmlFor="name"> Realtor Name</label>
-                    </div>
-                    <div>
-                        <input className="form-input" onChange={props.handleChange} name="name" type="text" value={props.realtor.name} />
-                    </div>
-                    <div>
-                        <label htmlFor="address">Address</label>
-                    </div>
-                    <div>
-                        <input onChange={props.handleChange} name="address" type="text" value={props.realtor.address} />
-                    </div>
-                    <div>
-                        <label htmlFor="city">City</label>
-                    </div>
-                    <div>
-                        <input onChange={props.handleChange} name="city" type="text" value={props.realtor.city} />
-                    </div>
-                    <div>
-                        <label htmlFor="state">State</label>
-                    </div>
-                    <div>
-                        <input onChange={props.handleChange} name="state" type="text" value={props.realtor.state} />
-                    </div>
-                    <div>
-                        <SubmitButton />
-                    </div>
-
-                </form>
-            </div>
+            </form>
         </div>
-    )
+    </div>
+)
 }
 
 export default NewRaeltor
