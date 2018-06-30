c9f23f43a419c468130934d7f2e908694f7cb78e
faisel-burqawi <faisel.burqawi@gmail.com>
2018-06-30 17:53:46 -0400

    listings-list

---
+++ client/src/components/ListingsList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function ListingsList (props) {
+
+    props.community.listings.map((listing, index) => {
+        return (<div key={index}>div</div>)
+    }
+
+    return (
+        <div>
+            test
+        </div>
+    )
+}
+
